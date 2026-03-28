be0e1967de62a324d9e92d295743178ff6fff034
Emre Armagan <emrearmagan.dev@gmail.com>
2026-03-28 20:47:45 +0100

    refactor init setup

--- lua/atlas/init.lua
+++ lua/atlas/init.lua
@@ -1,38 +1,44 @@
 local M = {}
 local logger = require("atlas.core.logger")
 
-local bootstrapped = false
-
 M.setup = function(opts)
 	require("atlas.config").setup(opts)
 	require("atlas.core.logger").clear()
 end
 
-local function bootstrap()
-	if bootstrapped then
-		return
-	end
-
+local function bootstrap_common()
 	require("atlas.ui.highlights").setup()
 	require("atlas.ui.components.footer").setup()
-	require("atlas.jira").setup()
-	require("atlas.bitbucket").setup()
-	require("atlas.github").setup()
 
 	require("atlas.ui.popups.help").register_keys("Commands", {
 		{ key = ":AtlasBitbucket", desc = "Open Bitbucket picker" },
 		{ key = ":AtlasJira", desc = "Open Jira picker" },
 		{ key = ":AtlasGithub", desc = "Open Github picker" },
 	}, { index = 999 })
+end
 
-	bootstrapped = true
+---@param view "jira"|"bitbucket"|"github"
+local function bootstrap_provider(view)
+	if view == "bitbucket" then
+		require("atlas.bitbucket").setup()
+		return
+	end
+
+	if view == "github" then
+		require("atlas.github").setup()
+		return
+	end
+
+	require("atlas.jira").setup()
 end
 
 function M.open(view)
 	logger.loginfo("Atlas open requested", { view = view })
+
 	local layout = require("atlas.ui.layout")
 	layout.ensure_open()
-	bootstrap()
+	bootstrap_common()
+	bootstrap_provider(view)
 	layout.open(view)
 
 	--FIX: This removes the statusline but is there a better way to do this? Could also effect other plugins that rely on the statusline
