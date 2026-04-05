0f23b9d34d63f05f7aa7f59183b67cf8ea67cf3c
Emre Armagan <emrearmagan.dev@gmail.com>
2026-04-05 05:24:12 +0200

    update conig

--- lua/atlas/health.lua
+++ lua/atlas/health.lua
@@ -63,8 +63,8 @@ end
 function M.check()
 	--- Requirements
 	vim.health.start("Requirements")
-	if vim.fn.has("nvim-0.9") == 0 then
-		vim.health.error("Neovim >= 0.9 required")
+	if vim.fn.has("nvim-0.10") == 0 then
+		vim.health.error("Neovim >= 0.10 required")
 	else
 		vim.health.ok("Neovim version compatible")
 	end
